a44419c118a252feacdece6a07c1b749296509a5
Mali Matiso <s220651337@mandela.ac.za>
2022-04-12 23:25:39 +0200

    Add function to retrieve all names

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -16,6 +16,7 @@ contract Domains is ERC721URIStorage {
 
     mapping(string => address) public domains;
     mapping(string => string) public records;
+    mapping (uint => string) public names;
 
     string public tld;
 
@@ -66,6 +67,7 @@ contract Domains is ERC721URIStorage {
         _setTokenURI(newRecordId, finalTokenUri);
         domains[name] = msg.sender;
 
+        names[newRecordId] = name;
         _tokenIds.increment();
     }
 
@@ -108,5 +110,16 @@ contract Domains is ERC721URIStorage {
 
         (bool success, ) = msg.sender.call{value: amount}("");
         require(success, "Failed to withdraw Matic");
+    }
+
+    function getAllNames() public view returns (string[] memory) {
+        console.log("Getting all names from contract");
+        string[] memory allNames = new string[](_tokenIds.current());
+        for (uint i = 0; i < _tokenIds.current(); i++) {
+            allNames[i] = names[i];
+            console.log("Name for token %d is %s", i, allNames[i]);
+        }
+
+        return allNames;
     }
 }
